49e6b55141425778e62a1e0623004a2b14a656bb
Vidhi-TEST <test9.vidhi@gmail.com>
2024-10-23 11:06:16 +0530

    CDT-14 (task) Optimize linear search function

--- c/algorithms/linearsearch.c
+++ c/algorithms/linearsearch.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
-int linsearch(char **yarr, char *val)
+int linsearch(char **yarr, char *val, int size)
 {
-    for(int i = 0; i < sizeof(yarr); i++)
+    for(int i = 0; i < size; i++)
     {
         if(strcmp(yarr[i], val) == 0)
         {
@@ -16,5 +17,6 @@ int main()
 {
     char *yarr[] = {"Shluck much", "much Shluck"};
     char *val = "Shluck Much";
-    linsearch(yarr, val);
+    int size = sizeof(yarr) / sizeof(yarr[0]);
+    linsearch(yarr, val, size);
 }
