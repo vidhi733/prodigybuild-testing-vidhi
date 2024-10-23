99e8630015259e6b910bf67b31252f3f24c59cc6
Vidhi-TEST <test9.vidhi@gmail.com>
2024-10-23 11:27:40 +0530

    CDT-18 (task) Optimize code for merge sort algorithm

--- c/algorithms/mergesort.c
+++ c/algorithms/mergesort.c
@@ -3,51 +3,45 @@
 #include<stdlib.h>
 
 // function to merge the sub-arrays
-void merge(int a[],int low,int mid ,int high)
-{
-	int b[20]; //same size of a[]
-	int i, j, k;
-	i = low, j = mid + 1,k = low;
-	while(i <= mid && j <= high)
-	{
-		if(a[i]<=a[j])
-		    b[k++]=a[i++];
-		else
-		   b[k++]=a[j++]; //copying the elements 
-	}
-	while (i<=mid)
-		b[k++]=a[i++];
-	while 
-		(j<=high) b[k++]=a[j++];
-		for (k=low;k<=high;k++)
-	        a[k]=b[k];
+void merge(int a[], int low, int mid, int high) {
+    int b[20]; //same size of a[]
+    int i, j, k;
+    i = low, j = mid + 1, k = low;
+    while (i <= mid && j <= high) {
+        if (a[i] <= a[j])
+            b[k++] = a[i++];
+        else
+            b[k++] = a[j++]; //copying the elements 
+    }
+    while (i <= mid)
+        b[k++] = a[i++];
+    while (j <= high)
+        b[k++] = a[j++];
+    for (k = low; k <= high; k++)
+        a[k] = b[k];
 }
 
 // merge sort function
-void mergesort(int a[],int low,int high)
-{
-	int mid;
-	if(low>=high)
-	  return;
-	mid=(low+high)/2;
-	mergesort(a,low,mid);
-	mergesort(a,mid+1,high);
-	merge(a,low,mid,high);
+void mergesort(int a[], int low, int high) {
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    mergesort(a, low, mid);
+    mergesort(a, mid + 1, high);
+    merge(a, low, mid, high);
 }
 
-// main fucntion
-int main()
-{
-	int a[7] = {83, 20, 9, 50, 115, 61, 17};
-	int n = 7;
+// main function
+int main() {
+    int a[7] = {83, 20, 9, 50, 115, 61, 17};
+    int n = 7;
 
-	mergesort(a, 0, n-1);
-	
-	printf("\n Sorted numbers are: ");
+    mergesort(a, 0, n - 1);
 
-	// function to print the sorted array
-	int k;
-	for(k = 0; k < 7; k++)
-	    printf("%d, ",a[k]);
-	return 0;
+    printf("\n Sorted numbers are: ");
+
+    // function to print the sorted array
+    for (int k = 0; k < 7; k++)
+        printf("%d, ", a[k]);
+    return 0;
 }
