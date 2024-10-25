394e6904497e289b68c8d876f8d6a7c934ac8e25
Vidhi-TEST <test9.vidhi@gmail.com>
2024-10-25 17:24:46 +0530

    GHD-1 (task) Write code for reverse pyramid start pattern

--- c/algorithms/pyramid.c
+++ c/algorithms/pyramid.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+void reverse_pyramid(int rows) {
+    int i, j, space;
+
+    for (i = rows; i >= 1; i--) {
+        for (space = 1; space <= rows - i; space++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
+
 void pyramid(int rows) {
     int i, j, space;
 
@@ -21,5 +37,6 @@ int main() {
     printf("Enter the number of rows: ");
     scanf("%d", &rows);
     pyramid(rows);
+    reverse_pyramid(rows - 1);
     return 0;
 }
