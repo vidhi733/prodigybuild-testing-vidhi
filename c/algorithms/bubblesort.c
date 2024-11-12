e67106aaf2f30b413ed11021dcc0b684ff91b496
Vidhi-TEST <test9.vidhi@gmail.com>
2024-11-12 15:07:36 +0530

    DP-12 (task) refactor bubble sort algorithm in c language

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-void bubble_sort(int *array, int len) 
-{
+void bubble_sort(int *array, int len) {
     for (int i = 0; i < len; i++) {
         for (int j = 0; j < len - i - 1; j++) {
             if (array[j] > array[j + 1]) {
@@ -13,8 +12,7 @@ void bubble_sort(int *array, int len)
     }
 }
 
-int main(void) 
-{
+int main(void) {
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
     
     bubble_sort(yarr, 10);
