057b73a95f28a1d9c11033f1137680434918e5a8
vidhi733 <81381709+vidhi733@users.noreply.github.com>
2025-01-21 11:15:48 +0530

    SCG-15 (bug) 🚨 [SonarCloud] c/algorithms/BST.c : 3 Issue(s) Detected

--- c/algorithms/BST.c
+++ c/algorithms/BST.c
@@ -38,13 +38,13 @@ int Search(struct BST* RootPtr, int item) { /*Implemented search using recursion
     } else if(item == RootPtr->data) {
         return 1; /*Returns 1 when element found*/
     } else if(item < RootPtr->data) {
-        Search(RootPtr->left, item); /*Otherwise search in left side of binary tree if searching value is less then the current node value*/
+        return Search(RootPtr->left, item); /*Otherwise search in left side of binary tree if searching value is less then the current node value*/
     } else {
-        Search(RootPtr->right, item); /*Otherwise search in right side of binary tree if searching value is greater then the current node value*/
+        return Search(RootPtr->right, item); /*Otherwise search in right side of binary tree if searching value is greater then the current node value*/
     }
 }
 
-void main() {
+int main() {
     struct BST* RootPtr = NULL;
     int item, cont, key;
     do {
@@ -64,4 +64,5 @@ void main() {
     } else {
         printf("\nNot Found\n");
     }
+    return 0;
 }
