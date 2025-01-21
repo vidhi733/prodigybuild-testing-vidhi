f1097bb1b48f5c864e137c4e167333e70d4b18e7
vidhi733 <81381709+vidhi733@users.noreply.github.com>
2025-01-21 11:15:30 +0530

    SCG-14 (bug) 🚨 [SonarCloud] c/algorithms/selection_sort.c : 4 Issue(s) Detected

--- c/algorithms/selection_sort.c
+++ c/algorithms/selection_sort.c
@@ -6,9 +6,9 @@ void print_arr(int *ptr, int size)
     putchar('[');
     while(size--)
     {
-	printf("%d", *ptr++);
-	if(size)
-	    putchar(',');
+        printf("%d", *ptr++);
+        if(size)
+            putchar(',');
     }
     printf("]\n");
 }
@@ -29,9 +29,9 @@ int *find_min(int *ptr, int size)
     min = ptr;
     while(size--)
     {
-	if(*ptr < *min)
-	    min = ptr;
-	ptr++;
+        if(*ptr < *min)
+            min = ptr;
+        ptr++;
     }
     return (min);
 }
@@ -42,12 +42,12 @@ void selection_sort(int *ptr, int size)
 
     while(--size)
     {
-	if((min = find_min(ptr + 1, size)))
-	{
-	    if(*ptr > *min)
-		swap(ptr, min);
-	}
-	ptr++;
+        if((min = find_min(ptr + 1, size)))
+        {
+            if(*ptr > *min)
+                swap(ptr, min);
+        }
+        ptr++;
     }
 }
 
@@ -57,26 +57,26 @@ void fill(char **av, int *ptr, int size)
 
     i = 2;
     while(av[i] && size--)
-	*ptr++ = atoi(av[i++]); 
+        *ptr++ = atoi(av[i++]);
 }
 
 int main(int argc, char *argv[])
 {
     if(argc < 3)
     {
-	puts("Usage: ./your-executable-name [array size] [array]");
-	puts("Example: ./your-executable-name 3 2 1 0");
-	return EXIT_FAILURE;
+        puts("Usage: ./your-executable-name [array size] [array]");
+        puts("Example: ./your-executable-name 3 2 1 0");
+        return EXIT_FAILURE;
     }
     int size = atoi(argv[1]);
     if(!size)
     {
-	puts("Error: size of array can't be 0");
-	return EXIT_FAILURE;
+        puts("Error: size of array can't be 0");
+        return EXIT_FAILURE;
     }
     int *arr = (int *)malloc(size * sizeof(int));
     if(!arr)
-	return EXIT_FAILURE;
+        return EXIT_FAILURE;
     fill(argv, arr, size);
 
     printf("Before sorting: ");
