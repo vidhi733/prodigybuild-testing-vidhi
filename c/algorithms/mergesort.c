ccbe6464f19f6a0dd4e068c2696635049a883794
vidhi733 <81381709+vidhi733@users.noreply.github.com>
2025-01-21 11:24:53 +0530

    PRC-1 (task) Refactor the code of merge sort

--- c/algorithms/mergesort.c
+++ c/algorithms/mergesort.c
@@ -1,69 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-struct BST {
-    int data;
-    struct BST* left;
-    struct BST* right;
-};
+void swap(int *a, int *b);
+int partition(int *a, int l, int h);
+void quick_sort(int *a, int l, int h);
 
-struct BST* CreateNode() {
-    struct BST* new = (struct BST*)malloc(sizeof(struct BST));
-    new->left = NULL;
-    new->right = NULL;
-    return new;
+void swap(int *a, int *b) {
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-void Insert(struct BST** RootPtr, int value) {
-    struct BST* temp = *RootPtr;
-    if (temp == NULL) {
-        /*When list is empty*/
-        struct BST* NewNode = CreateNode();
-        NewNode->data = value;
-        *RootPtr = NewNode;
-    } else if (value <= temp->data) {
-        /*If user value is less then current node value insert in left of the node...*/
-        struct BST* NewNode = CreateNode();
-        NewNode->data = value;
-        temp->left = NewNode;
-    } else {
-        /*If user value is greater then current node value insert at right of the node*/
-        struct BST* NewNode = CreateNode();
-        NewNode->data = value;
-        temp->right = NewNode;
+int partition(int a[], int l, int h) {
+    int i = l, j = l, p = h;
+    while (i < h) {
+        if (a[i] < a[p]) {
+            swap(&a[i], &a[j]);
+            j++;
+        }
+        i++;
     }
+    swap(&a[p], &a[j]);
+    return j;
 }
 
-int Search(struct BST* RootPtr, int item) {
-    /*Implemented search using recursion*/
-    if (RootPtr == NULL) {
-        return 0; /*Returns 0 if list is empty*/
-    } else if (item == RootPtr->data) {
-        return 1; /*Returns 1 when element found*/
-    } else if (item < RootPtr->data) {
-        return Search(RootPtr->left, item); /*Otherwise search in left side of binary tree if searching value is less then the current node value*/
-    } else {
-        return Search(RootPtr->right, item); /*Otherwise search in right side of binary tree if searching value is greater then the current node value*/
+void quick_sort(int a[], int l, int h) {
+    int p;
+    if (l < h) {
+        p = partition(a, l, h);
+        quick_sort(a, l, p - 1);
+        quick_sort(a, p + 1, h);
     }
 }
 
 int main() {
-    struct BST* RootPtr = NULL;
-    int item, cont, key;
-    do {
-        printf("Enter item: ");
-        scanf("%d", &item);
-        Insert(&RootPtr, item);
-        printf("\n1 to keep inserting/ 0 to Exit: ");
-        scanf("%d", &cont);
-    } while (cont == 1);
-    printf("\nEnter element to search: ");
-    scanf("%d", &key);
-    if (Search(RootPtr, key) == 0) {
-        printf("\nFound\n");
-    } else {
-        printf("\nNot Found\n");
+    int size;
+    printf("Size: ");
+    scanf("%d", &size);
+    int *arr = malloc(size * sizeof(int));
+    printf("Array elements: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
     }
+    quick_sort(arr, 0, size - 1);
+    printf("Sorted array:");
+    for (int i = 0; i < size; i++) {
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
+    free(arr);
     return 0;
 }
