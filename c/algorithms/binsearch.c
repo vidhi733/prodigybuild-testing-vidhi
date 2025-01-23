669e313558affee2f933c4c662c0343709f8de5d
vidhi733 <81381709+vidhi733@users.noreply.github.com>
2025-01-23 16:06:50 +0530

    NP-7 (bug) 🚨 [SonarCloud] c/algorithms/binsearch.c : 4 Issue(s) Detected

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int binsearch(int yarr[10], int element)
+int binsearch(const int *yarr, int element)
 {
-    int mid = sizeof(yarr) / sizeof(int) / 2;
+    int mid = 10 / 2;
     int left = mid;
     int right = 10 - left;
     extern int i;
-    
-    if(element == yarr[mid])
+
+    if (element == yarr[mid])
     {
         printf("%d", yarr[mid]);
     }
-    
-    if(element != yarr[right])
+
+    if (element != yarr[right])
     {
-        for(i = 0; i < left; i++)
+        for (i = 0; i < left; i++)
         {
-            if(element == yarr[i])
+            if (element == yarr[i])
             {
                 printf("%d", yarr[i]);
             }
@@ -27,9 +28,9 @@ int binsearch(int yarr[10], int element)
 int main(int argc, char *argv[])
 {
     int yarr[10];
-    for(int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
     {
-        yarr[i] = atoi(argv[i+1]);
+        yarr[i] = atoi(argv[i + 1]);
     }
     int element = atoi(argv[11]);
     binsearch(yarr, element);
